010471c2a68c0ca17637fc1fd2a8ebdedf23c548
KRISHNA-5478 <krishnatripathi07042005@gmail.com>
2025-05-12 04:07:41 +0530

    script

---
+++ add_contributor.bat
@@ -0,0 +1,30 @@
+@echo off
+REM This script adds a contributor to the repository history
+REM Usage: add_contributor.bat "Contributor Name" "contributor@email.com"
+
+if "%~1"=="" goto usage
+if "%~2"=="" goto usage
+
+set CONTRIBUTOR_NAME=%~1
+set CONTRIBUTOR_EMAIL=%~2
+
+REM Create a temporary commit to add the contributor
+echo # Adding %CONTRIBUTOR_NAME% as a contributor > temp_contributor.md
+git add temp_contributor.md
+git config --local user.name "%CONTRIBUTOR_NAME%"
+git config --local user.email "%CONTRIBUTOR_EMAIL%"
+git commit -m "Adding %CONTRIBUTOR_NAME% as a contributor"
+git rm temp_contributor.md
+git commit -m "Cleanup temporary file"
+
+REM Reset to original user
+git config --local --unset user.name
+git config --local --unset user.email
+
+echo Contributor %CONTRIBUTOR_NAME% has been added to the repository history.
+echo Don't forget to push the changes with: git push origin main
+goto :eof
+
+:usage
+echo Usage: %0 "Contributor Name" "contributor@email.com"
+exit /B 1
